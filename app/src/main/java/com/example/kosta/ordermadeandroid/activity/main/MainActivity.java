e50d6b25a4a9083e78bc090e97138d900c79190d
csi <ckthdlr@naver.com>
2017-06-07 09:25:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kosta/ordermadeandroid/activity/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kosta.ordermadeandroid.activity.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kosta.ordermadeandroid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
