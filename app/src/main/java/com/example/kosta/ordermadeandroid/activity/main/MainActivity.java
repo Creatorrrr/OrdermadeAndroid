69002f0c0da388f0a4610b07802c1d89f1d8630c
paulsikmin <forloginservice@gmail.com>
2017-06-08 12:37:15 +0900

    17/06/08 first push, navigation View

--- app/src/main/java/com/example/kosta/ordermadeandroid/activity/main/MainActivity.java
+++ app/src/main/java/com/example/kosta/ordermadeandroid/activity/main/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.kosta.ordermadeandroid.activity.main;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.kosta.ordermadeandroid.R;
@@ -11,15 +17,46 @@ import com.example.kosta.ordermadeandroid.activity.portfolio.Register;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView view = (NavigationView)findViewById(R.id.navigation_view);
+
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
     }
 
-        public void click(View v){
-            Intent intent = new Intent(getApplicationContext(), Register.class);
-            startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
