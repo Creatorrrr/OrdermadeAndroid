e34defcfefb6cf58751b936acf38d60aa18441a1
eomjaeun <flutter.redien@gmail.com>
2017-06-13 15:36:19 +0900

    Merge remote-tracking branch 'remotes/origin/zun' into eom0608
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/activity_product_register.xml

--- app/src/main/java/com/example/kosta/ordermadeandroid/activity/product/ProductRegisterActivity.java
+++ app/src/main/java/com/example/kosta/ordermadeandroid/activity/product/ProductRegisterActivity.java
@@ -1,27 +1,46 @@
 package com.example.kosta.ordermadeandroid.activity.product;
 
+import android.databinding.DataBindingUtil;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.kosta.ordermadeandroid.R;
+import com.example.kosta.ordermadeandroid.databinding.ProductRegisterDataBinding;
+import com.example.kosta.ordermadeandroid.dto.Product;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ProductRegisterActivity extends AppCompatActivity {
-
+    public static final String TAG = ProductRegisterActivity.class.getSimpleName();
     private Toolbar mToolbar;
+    private Product product;
+    ProductRegisterDataBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_register);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_register);
+        product = new Product();
+        binding.setProduct(product);
+        binding.setActivity(this);
 
-        mToolbar = (Toolbar)findViewById(R.id.actionbar_productRegister);
+
+        mToolbar = (Toolbar) findViewById(R.id.actionbar_productRegister);
         mToolbar.setTitle("상품 등록");
         //mToolbar.setTitleTextColor(Color.WHITE);
 
@@ -33,11 +52,37 @@ public class ProductRegisterActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onButtonClick(String title, String content) {
+        Log.d(TAG, "Title :" + binding.getProduct().getTitle());
+        Log.d(TAG, "Content :" + binding.getProduct().getContent());
+        new productRegisterTask().execute("http://10.0.2.2:8080/")
+
+    }
+
+
+    private class productRegisterTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection http = null;
+            InputStream is = null;
+            try {
+                URL url = new URL(params[0]);
+                http = (HttpURLConnection) url.openConnection();
+                http.setRequestMethod("POST");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
